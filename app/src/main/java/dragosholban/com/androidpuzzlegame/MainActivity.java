df0c22355842fd842e8c2cb322a36f8a45090129
JoseAntonioNuevo <joseantonionuevo@gmail.com>
2020-11-30 23:18:28 +0100

    Producto 2

--- app/src/main/java/dragosholban/com/androidpuzzlegame/MainActivity.java
+++ app/src/main/java/dragosholban/com/androidpuzzlegame/MainActivity.java
@@ -1,42 +1,33 @@
 package dragosholban.com.androidpuzzlegame;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-
-import static java.lang.Math.abs;
 
 public class MainActivity extends AppCompatActivity {
     private static final String EXTRA_MESSAGE = "";
@@ -45,36 +36,58 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_PERMISSION_READ_EXTERNAL_STORAGE = 3;
     static final int REQUEST_IMAGE_GALLERY = 4;
-    int nivelgame=1;
+    int nivelgame = 1;
+
+    Button play_pause, btn_repetir;
+    public MediaPlayer mp;
+    int repetir = 2, posicion = 0;
+    MediaPlayer vectormp[] = new MediaPlayer[3];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ;
+        play_pause = (Button) findViewById(R.id.btn_play);
+        btn_repetir = (Button) findViewById(R.id.btn_repetir);
+
+        vectormp[0] = MediaPlayer.create(this, R.raw.long_song);
+        vectormp[1] = MediaPlayer.create(this, R.raw.c2c);
+        vectormp[2] = MediaPlayer.create(this, R.raw.mind);
+
         AssetManager am = getAssets();
+
+        mp = MediaPlayer.create(this, R.raw.long_song);
+        mp.start();
+        TelephonyManager mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        mTelephonyManager.listen(mPh, PhoneStateListener.LISTEN_CALL_STATE);
+        AudioManager mAudioManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        AudioFocusChangeListenerImpl mAudioFocusChange= new AudioFocusChangeListenerImpl();
+        int result = mAudioManager.requestAudioFocus(mAudioFocusChange, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+
         try {
-            final String[] files  = am.list("img");
+            final String[] files = am.list("img");
 
             GridView grid = findViewById(R.id.grid);
             grid.setAdapter(new ImageAdapter(this));
             grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    switch (i){
+                    switch (i) {
                         case 0:
-                            PuzzleActivity.nivel=1;
+                            PuzzleActivity.nivel = 1;
                             break;
                         case 1:
-                            PuzzleActivity.nivel=2;
+                            PuzzleActivity.nivel = 2;
                             break;
                         case 2:
-                            PuzzleActivity.nivel=3;
+                            PuzzleActivity.nivel = 3;
                             break;
                         case 3:
-                            PuzzleActivity.nivel=4;
+                            PuzzleActivity.nivel = 4;
                             break;
                         case 4:
-                            PuzzleActivity.nivel=4;
+                            PuzzleActivity.nivel = 4;
                             break;
                     }
                     Intent intent = new Intent(getApplicationContext(), PuzzleActivity.class);
@@ -87,13 +100,15 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT);
         }
-        nivelgame=nivelgame+1;
+        nivelgame = nivelgame + 1;
     }
-    public void onhelpbutton(View view){
+
+    public void onhelpbutton(View view) {
         Intent intent = new Intent(this, Web.class);
 
         startActivity(intent);
     }
+
     public void onImageFromCameraClick(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (intent.resolveActivity(getPackageManager()) != null) {
@@ -151,13 +166,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Intent intent = new Intent(this, PuzzleActivity.class);
-            PuzzleActivity.nivel=1;
+            PuzzleActivity.nivel = 1;
             intent.putExtra("mCurrentPhotoPath", mCurrentPhotoPath);
             startActivity(intent);
         }
         if (requestCode == REQUEST_IMAGE_GALLERY && resultCode == RESULT_OK) {
             Uri uri = data.getData();
-            PuzzleActivity.nivel=1;
+            PuzzleActivity.nivel = 1;
             Intent intent = new Intent(this, PuzzleActivity.class);
             intent.putExtra("mCurrentPhotoUri", uri.toString());
             startActivity(intent);
@@ -166,11 +181,145 @@ public class MainActivity extends AppCompatActivity {
 
     public void onImageFromGalleryClick(View view) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_READ_EXTERNAL_STORAGE);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_READ_EXTERNAL_STORAGE);
         } else {
             Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
             intent.setType("image/*");
             startActivityForResult(intent, REQUEST_IMAGE_GALLERY);
         }
     }
+    //funciones para la música en el juego
+
+    public void Volver(View view) {
+        Intent volver = new Intent(this, MainActivity.class);
+        startActivity(volver);
+    }
+
+    public void PlayPause(View view) {
+        mp.stop();
+        if (vectormp[posicion].isPlaying()) {
+            vectormp[posicion].pause();
+            play_pause.setBackgroundResource(R.drawable.reproducir);
+            Toast.makeText(this, "Pausa", Toast.LENGTH_SHORT).show();
+        } else {
+            vectormp[posicion].start();
+            play_pause.setBackgroundResource(R.drawable.pausa);
+            Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void Stop(View view) {
+        mp.stop();
+        if (vectormp[posicion] != null) {
+            vectormp[posicion].stop();
+
+            vectormp[0] = MediaPlayer.create(this, R.raw.long_song);
+            vectormp[1] = MediaPlayer.create(this, R.raw.c2c);
+            vectormp[2] = MediaPlayer.create(this, R.raw.mind);
+            posicion = 0;
+            play_pause.setBackgroundResource(R.drawable.reproducir);
+            Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public void Repetir(View view) {
+        if (repetir == 1) {
+            btn_repetir.setBackgroundResource(R.drawable.no_repetir);
+            Toast.makeText(this, "No repetir", Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping(false);
+            repetir = 2;
+        } else {
+            btn_repetir.setBackgroundResource(R.drawable.repetir);
+            Toast.makeText(this, "Repetir", Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping(true);
+            repetir = 1;
+        }
+    }
+
+    public void Siguiente(View view) {
+        if (posicion < vectormp.length - 1) {
+            if (vectormp[posicion].isPlaying()) {
+                vectormp[posicion].stop();
+                posicion++;
+                vectormp[posicion].start();
+            } else {
+                posicion++;
+            }
+
+        } else {
+            Toast.makeText(this, "No hay mas canciones", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void Anterior(View view) {
+        if (posicion >= 1) {
+            if (vectormp[posicion].isPlaying()) {
+                vectormp[posicion].stop();
+                vectormp[0] = MediaPlayer.create(this, R.raw.long_song);
+                vectormp[1] = MediaPlayer.create(this, R.raw.c2c);
+                vectormp[2] = MediaPlayer.create(this, R.raw.mind);
+                posicion--;
+                vectormp[posicion].start();
+            } else {
+                posicion--;
+            }
+
+        } else {
+            Toast.makeText(this, "No hay mas canciones", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private final PhoneStateListener mPh = new PhoneStateListener() {
+        // @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+            try {
+                switch (state) {
+                    case TelephonyManager.CALL_STATE_RINGING:
+                        vectormp[posicion].pause();
+                        mp.pause();
+                        break;
+
+                    case TelephonyManager.CALL_STATE_OFFHOOK:
+                        break;
+
+                    case TelephonyManager.CALL_STATE_IDLE:
+                        vectormp[posicion].start();
+                        mp.start();
+                        break;
+                    default:
+                }
+            } catch (Exception e) {
+
+
+            }
+        }
+    };
+    
+    private class AudioFocusChangeListenerImpl implements AudioManager.OnAudioFocusChangeListener{
+        @Override
+        public void onAudioFocusChange(int focusChange){
+            switch (focusChange){
+                case AudioManager.AUDIOFOCUS_GAIN:
+                    vectormp[posicion].start();
+                    mp.start();
+                    break;
+                case AudioManager.AUDIOFOCUS_LOSS:
+                    vectormp[posicion].pause();
+                    mp.pause();
+                    break;
+                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                    vectormp[posicion].pause();
+                    mp.pause();
+                    break;
+                case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK:
+                    vectormp[posicion].pause();
+                    mp.pause();
+                    break;
+            }
+        }
+    }
+
 }
--- app/src/main/java/dragosholban/com/androidpuzzlegame/Web.java
+++ app/src/main/java/dragosholban/com/androidpuzzlegame/Web.java
@@ -1,7 +1,7 @@
 package dragosholban.com.androidpuzzlegame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
